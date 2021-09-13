cd07a52bb09d363133865f0a89ad49a9725d904c
anishchaudhary27 <anishchaudhary2706@gmail.com>
2021-09-13 22:11:21 +0530

    removed key

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -4,16 +4,7 @@ import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-
-const firebaseConfig = {
-    apiKey: "AIzaSyCA8Z8emKA3yfPOQjj5YV9SK8JbR3d8Exw",
-    authDomain: "trible-2706.firebaseapp.com",
-    projectId: "trible-2706",
-    storageBucket: "trible-2706.appspot.com",
-    messagingSenderId: "1075956632820",
-    appId: "1:1075956632820:web:61eb6bbf34a4c1f8dfa2a1",
-    measurementId: "G-76BZJ2HMDH"
-};
+import { firebaseConfig } from '../FirebaseKey.js'
 
 const app = initializeApp(firebaseConfig);
 if (window.location.hostname !== 'localhost') {
